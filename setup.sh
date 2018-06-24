f83e809788a2a407dc74b04fafafa42384072edd
Jon Medina <jonmedina@jdam1228.local>
2018-06-23 22:03:06 -0700

    Updated to allow dockerfile usage

--- setup.sh
+++ setup.sh
@@ -22,33 +22,17 @@ This script will now download the above software using apt-get and pip3. If you'
 NOTE: sdnpwn works best with Python versions 3.4 and 3.5. Some modules may not function correctly when other Python versions are used.
 
 "
-read -p "Install Required Software? [y/N] " res
-
-if [ "$res" == "y" ]; then
   apt-get update
-  apt-get install python3 python3-pip python3-netifaces python3-scipy git bridge-utils python3-tabulate
+  apt-get install -y python3 python3-pip python3-netifaces python3-scipy git bridge-utils python3-tabulate
   pip3 install scapy-python3
   #pip3 install python-openflow
   pip3 install websocket-client
-  
+
   mkdir lib
   cd lib
   git clone https://github.com/smythtech/python-openflow-legacy
   cd python-openflow-legacy
   chmod +x setup.py
   sudo python3 setup.py install
-  cd ..  
-
-echo -e "\n
-
-Maven is used by some modules to build Java-based SDN applications"
-
-  read -p "Would you like to install Maven? [y/N] " res
-
-  if [ "$res" == "y" ]; then
-    apt-get install maven
-  fi
-
-else
-  echo "Quiting."
-fi
+  cd ..
+  apt-get install -y maven
